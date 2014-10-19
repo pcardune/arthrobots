c13975e616d90f515af972a55c2e1b545a2d2994
Paul Carduner <paul@carduner.net>
2014-10-19 16:09:44 -0700

    Basic language definition with some tests ported over from gvr

---
+++ src/lang/__tests__/lang-test.js
@@ -0,0 +1,209 @@
+jest.dontMock('../../core/Class');
+jest.dontMock('../lang');
+
+describe('lang Expression', function() {
+
+  beforeEach(function(){
+    gvr = {lang:require('../lang')};
+  });
+
+  /**
+   * The most atomic part of a code execution path is an
+   * expression. An expression simply calls a passed in function with a
+   * given scope and returns an empty stack (i.e. there is nothing left
+   * to do.)
+   */
+  it('should call the associated function and return an empty stack',
+    function() {
+      var lang = require('../lang');
+      var calledWith;
+      var scope = {a: 1};
+      function func(){
+        calledWith = this;
+      }
+      var expression = gvr.lang.newExpression(1, func, scope);
+      var next = expression.step();
+      expect(calledWith).toBe(scope);
+      expect(next.length).toBe(0);
+    });
+
+  /**
+   * In the event that calling the function passed to the
+   * expression causes an error to be thrown, the error will be caught
+   * and an alert presented to the user.
+   */
+  it('should catch exceptions thrown in expression functions and throw an alert', function() {
+    var lang = require('../lang')
+    window.alert = jest.genMockFunction();
+    function func(){
+      throw new Error("some error");
+    }
+    var expression = gvr.lang.newExpression(1, func, {});
+    var next = expression.step();
+    expect(next).toBe(null);
+    expect(window.alert.mock.calls[0][0]).toBe("some error");
+  });
+
+  /**
+   * The next level up after expressions are blocks. These are
+   * essentially just a grouping of expression objects.
+   */
+  it('should work with blocks', function(){
+    var log = [];
+    var block = new gvr.lang.Block([
+      gvr.lang.newExpression(1, function(){ log.push(1); }, this)
+    ]);
+    // step over the block once, it will finish
+    expect(block.currentStep).toBe(0);
+    var next = block.step();
+    expect(log[0]).toBe(1);
+    expect(next.length).toBe(1);
+    expect(block.currentStep).toBe(1);
+    // if the block is finished, and we step onto it, it will reset.
+    next = block.step();
+    expect(log[0]).toBe(1);
+    expect(log.length).toBe(1);
+    expect(next.length).toBe(0);
+    expect(block.currentStep).toBe(0);
+    // now it goes back to the beginning.
+    expect(block.currentStep).toBe(0);
+    next = block.step();
+    expect(log[1]).toBe(1);
+    expect(next.length).toBe(1);
+    expect(block.currentStep).toBe(1);
+  });
+
+  it("should work with blocks that have multiple expressions", function() {
+    var log = [];
+    var block = new gvr.lang.Block(
+      [
+        gvr.lang.newExpression(1, function(){ log.push("one"); }, this),
+        gvr.lang.newExpression(2, function(){ log.push("two"); }, this),
+        gvr.lang.newExpression(3, function(){ log.push("three"); }, this)
+      ]);
+    // execute the first step
+    expect(block.currentStep).toBe(0);
+    var next = block.step();
+    expect(log[0]).toBe("one");
+    expect(next.length).toBe(1);
+    expect(next[0]).toBe(block);
+    expect(block.currentStep).toBe(1);
+    // execute the second step
+    next = block.step();
+    expect(log[1]).toBe("two");
+    expect(next.length).toBe(1);
+    expect(next[0]).toBe(block);
+    expect(block.currentStep).toBe(2);
+    // execute the third step
+    next = block.step();
+    expect(log[2]).toBe("three");
+    expect(next.length).toBe(1);
+    expect(block.currentStep).toBe(3);
+    // the next time we step on the block, it resets.
+    next = block.step();
+    expect(log.length).toBe(3);
+    expect(next.length).toBe(0);
+    expect(block.currentStep).toBe(0);
+  });
+
+  it("should work with if statements", function(){
+    var log = [];
+    var nextCond = true;
+    function condition(){
+      return nextCond;
+    }
+    var ifExpr = gvr.lang.newIf(1, condition, {},
+      [gvr.lang.newExpression(2, function(){ log.push("one"); }, this)]
+    );
+    expect(ifExpr.block.currentStep).toBe(0);
+    var next = ifExpr.step();
+    expect(next.length).toBe(1);
+    expect(log.length).toBe(1);
+    expect(log[0],"one");
+    expect(ifExpr.block.currentStep).toBe(1);
+    next[0].step();
+    expect(ifExpr.block.currentStep).toBe(0);
+    next = ifExpr.step();
+    expect(log.length).toBe(2);
+    next[0].step();
+    nextCond = false;
+    expect(ifExpr.block.currentStep).toBe(0);
+    next = ifExpr.step();
+    expect(log.length).toBe(2);
+  });
+
+  it("should work with else statements", function() {
+    var log = [];
+    var nextCond = true;
+    function condition(){
+      return nextCond;
+    }
+    var ifExpr = gvr.lang.newIf(1, condition, {}, [gvr.lang.newExpression(2, function(){ log.push("one"); }, this)]);
+    ifExpr.elseBlock.expressions.push(gvr.lang.newExpression(2, function(){ log.push("two"); }, this));
+    expect(ifExpr.block.currentStep).toBe(0);
+    var next = ifExpr.step();
+    expect(next.length).toBe(1);
+    expect(log.length).toBe(1);
+    expect(log[0],"one");
+    expect(ifExpr.block.currentStep).toBe(1);
+    next[0].step();
+    expect(ifExpr.block.currentStep).toBe(0);
+    nextCond = false;
+    expect(ifExpr.elseBlock.currentStep).toBe(0);
+    next = ifExpr.step();
+    expect(log.length).toBe(2);
+    expect(log[1],"two");
+    expect(ifExpr.elseBlock.currentStep).toBe(1);
+    expect(next.length).toBe(1);
+    next[0].step();
+    expect(ifExpr.elseBlock.currentStep).toBe(0);
+  });
+
+  it('should work with elif statements', function() {
+    var log = [];
+    var a = true;
+    var b = true;
+    var c = true;
+    var ifExpr = gvr.lang.newIf(      1, function(){return a;}, {}, [
+             gvr.lang.newExpression(2, function(){ log.push("a"); }, this)]);
+    ifExpr.elifs.push(
+           gvr.lang.newIf(      3, function(){return b;}, {}, [
+             gvr.lang.newExpression(4, function(){ log.push("b"); }, this)]));
+    ifExpr.elifs.push(
+           gvr.lang.newIf(      5, function(){return c;}, {}, [
+             gvr.lang.newExpression(6, function(){ log.push("c"); }, this)]));
+    // testing if catch
+    expect(ifExpr.block.currentStep).toBe(0);
+    var next = ifExpr.step();
+    expect(next.length).toBe(1);
+    expect(log.length).toBe(1);
+    expect(log[0],"a");
+    expect(ifExpr.block.currentStep).toBe(1);
+    next[0].step();
+    expect(ifExpr.block.currentStep).toBe(0);
+    // testing first elif catch
+    a = false;
+    expect(ifExpr.elifs[0].block.currentStep).toBe(0);
+    next = ifExpr.step();
+    expect(log.length).toBe(2);
+    expect(log[1],"b");
+    // testing second elif catch
+    b = false;
+    expect(ifExpr.elifs[1].block.currentStep).toBe(0);
+    next = ifExpr.step();
+    expect(log.length).toBe(3);
+    expect(log[2],"c");
+    c = false;
+    next = ifExpr.step();
+    expect(log.length).toBe(3);
+    expect(log[2],"c");
+    ifExpr.elseBlock.expressions.push(
+             gvr.lang.newExpression(7, function(){ log.push("else"); }, this));
+    // testing else catch
+    expect(ifExpr.elseBlock.currentStep).toBe(0);
+    next = ifExpr.step();
+    expect(log.length).toBe(4);
+    expect(log[3],"else");
+  });
+
+});
---
+++ src/lang/lang.js
@@ -0,0 +1,430 @@
+
+var Class = require('../core/Class');
+
+var gvr = {lang:{}};
+
+gvr.lang.RUNNER_GLOBAL_KEY = "$runner";
+
+/**
+ * @function
+ */
+gvr.lang.getRunner = function getRunner(globals){
+  globals = globals || {};
+  var runner = globals[gvr.lang.RUNNER_GLOBAL_KEY];
+  if (!runner){
+    /**
+     * stubbed out version of {@link gvr.runner.Runner}
+     */
+    runner = {
+      /**
+       * @see gvr.runner.Runner#notify
+       */
+      notify:function(){}
+    };
+  }
+  return runner;
+};
+
+gvr.lang.BaseExpression = Class.extend(
+  /** @lends gvr.lang.BaseExpression# */
+  {
+    /**
+     * the line number associated with this expression.
+     */
+    line: null,
+    /**
+     * the name of this expression. (mostly for debugging)
+     */
+    name: null,
+
+    /**
+     * @class base class used by all other language elements.
+     * @constructs
+     * @param line the line number associated with this expression.
+     * @param name the name of this expression. (mostly for debugging)
+     */
+    init: function(line, name){
+      this.line = line;
+      this.name = name;
+    }
+  }
+);
+
+gvr.lang.Expression = gvr.lang.BaseExpression.extend(
+  /** @lends gvr.lang.Expression# */
+  {
+
+    /**
+     * A callable that executes this expression.
+     */
+    callable: null,
+    /**
+     * The scope with which to call the callable.
+     */
+    scope: null,
+
+    /**
+     * @class Represents a single expression in the gvr language.
+     * @extends gvr.lang.BaseExpression
+     * @constructs
+     * @param line See {@link gvr.lang.BaseExpression#line}
+     * @param callable The callable function that constitutes execution of this expression.
+     * @param scope The scope with which to call the callable function.
+     */
+    init: function(line, callable, scope){
+      this._super(line, "expr");
+      this.callable = callable;
+      this.scope = scope;
+    },
+    /**
+     * Step over this expression.
+     * @param globals global values passed throughout program execution.
+     */
+    step: function(globals){
+      gvr.lang.getRunner(globals).notify(this);
+      try{
+        this.callable.call(this.scope);
+        return [];
+      } catch (e){
+        alert(e.message);
+      }
+      return null;
+    }
+  });
+
+/**
+ * Creates a new Expression object.
+ * @see gvr.lang.Expression
+ * @returns {gvr.lang.Expression}
+ */
+gvr.lang.newExpression = function(line, callable, scope){
+  return new gvr.lang.Expression(line, callable, scope);
+};
+
+
+gvr.lang.Block = Class.extend(
+  /** @lends gvr.lang.Block# */
+  {
+    /**
+     * @class Represents an executable grouping of {@link gvr.lang.BaseExpression} objects.
+     * @constructs
+     * @param expressions A list of {@link gvr.lang.BaseExpression} objects to step over.
+     */
+    init: function(expressions){
+      this.name="block";
+      /**
+       * A list of expressions that are part of the block
+       */
+      this.expressions = expressions;
+      /**
+       * the current step in the block
+       */
+      this.currentStep = 0;
+    },
+
+    /**
+     * Step over the next expression in the block.  Once all expressions
+     * in the block have been stepped over, the currentStep counter is
+     * reset to 0 so the same block instance may be steped over again
+     * (for example, when the block is part of a loop).
+     * @param globals global values passed throughout program execution.
+     */
+    step: function(globals){
+      var stack = [];
+      if (this.currentStep < this.expressions.length){
+        stack.push(this);
+        var next = this.expressions[this.currentStep].step(globals);
+        this.currentStep++;
+        stack = stack.concat(next);
+      } else {
+        this.currentStep = 0;
+      }
+      return stack;
+    }
+  });
+
+gvr.lang.If = gvr.lang.BaseExpression.extend(
+  /** @lends gvr.lang.If# */
+  {
+    /**
+     * @class Represents an if statement in the gvr language.
+     * @extends gvr.lang.BaseExpression
+     * @constructs
+     * @param line See {@link gvr.lang.BaseExpression#line}
+     * @param callable See {@link gvr.lang.If#callable}
+     * @param scope See {@link gvr.lang.If#scope}
+     * @param expressions A list of expressions with which to
+     *          create a {@link gvr.lang.Block} object
+     */
+    init: function (line, callable, scope, expressions){
+      this._super(line, "if");
+
+      /**
+       * The callable that returns a boolean value
+       * @type {function()}
+       */
+      this.callable = callable;
+
+      /**
+       * The scope with which to call the callable
+       * @type {Object}
+       */
+      this.scope = scope;
+
+      /**
+       * The block to step into if the callable returns true
+       * @type {gvr.lang.Block}
+       */
+      this.block = new gvr.lang.Block(expressions);
+
+      /**
+       * A list of other elif conditions to try.
+       * @type {Array}
+       */
+      this.elifs = [];
+
+      /**
+       * The block to step into if the callable returns false.
+       * @type {gvr.lang.Block}
+       */
+      this.elseBlock = new gvr.lang.Block([]);
+    },
+
+    /**
+     * Step into the appropriate block depending
+     * on the return value of {@link gvr.lang.If#callable}.
+     * If there {@link gvr.lang.If#elifs} is not empty, and
+     * {@link gvr.lang.If#callable} returns false, then all the elifs
+     * will be processed.
+     * @param globals global values passed throughout program execution.
+     */
+    step:function (globals){
+      gvr.lang.getRunner(globals).notify(this);
+      var conditionMatched = false;
+      var stack = [];
+      if (this.callable.call(this.scope)){
+        stack = this.block.step(globals);
+        conditionMatched = true;
+      } else if (this.elifs.length > 0){
+        for (var i=0; i < this.elifs.length; i++){
+          var stackLength = stack.length;
+          stack = this.elifs[i].step(globals);
+          if (stackLength != stack.length){
+            //Stack was changed so we shouldn't fall through anymore.
+            conditionMatched = true;
+            break;
+          }
+        }
+      }
+      if (!conditionMatched && this.elseBlock.expressions.length > 0){
+        stack = this.elseBlock.step(globals);
+      }
+      return stack;
+    }
+  });
+
+/**
+ * creates a new {@link gvr.lang.If} object
+ * @returns {gvr.lang.If}
+ */
+gvr.lang.newIf = function (line, callable, scope, expressions){
+  return new gvr.lang.If(line, callable, scope, expressions);
+};
+
+
+gvr.lang.While = gvr.lang.BaseExpression.extend(
+  /** @lends gvr.lang.While# */
+  {
+    /**
+     * @class Represents a while loop in the gvr language.
+     * @extends gvr.lang.BaseExpression
+     * @constructs
+     * @param line See {@link gvr.lang.BaseExpression#line}
+     * @param callable See {@link gvr.lang.If#callable}
+     * @param scope See {@link gvr.lang.If#scope}
+     * @param expressions A list of expressions with which to
+     *          create a {@link gvr.lang.Block} object
+     */
+    init: function(line, callable, scope, expressions){
+      this._super(line, "while");
+      /** A callable that returns a boolean value. */
+      this.callable = callable;
+      /** the scope with which to call the callable */
+      this.scope = scope;
+      /** The block to step into as long as the callable returns true */
+      this.block = new gvr.lang.Block(expressions);
+    },
+
+    /**
+     * Step into the while loop's block
+     * as long as {@link gvr.lang.While#callable} returns true
+     * @param globals global values passed throughout program execution.
+     */
+    step: function(globals){
+      gvr.lang.getRunner(globals).notify(this);
+      var stack = [];
+      if (this.callable.call(this.scope)){
+        stack.push(this);
+        stack = stack.concat(this.block.step(globals));
+      }
+      return stack;
+    }
+  });
+
+/**
+ * creates a new {@link gvr.lang.While} object
+ * @returns {gvr.lang.While}
+ */
+gvr.lang.newWhile = function(line, callable, scope, expressions){
+  return new gvr.lang.While(line, callable, scope, expressions);
+};
+
+gvr.lang.Do = gvr.lang.BaseExpression.extend(
+  /** @lends gvr.lang.Do# */
+  {
+    /**
+     * @class Represents a do loop in the gvr language.
+     * @extends gvr.lang.BaseExpression
+     * @constructs
+     * @param line See {@link gvr.lang.BaseExpression#line}
+     * @param count See {@link gvr.lang.Do#count}
+     * @param expressions A list of expressions with which to
+     *          create a {@link gvr.lang.Block} object
+     */
+    init: function(line, count, expressions){
+      this._super(line, "do");
+
+      /**
+       * The number of times to step into the block
+       * @type int
+       */
+      this.count = count;
+
+      /**
+       * The block to step into after each successive step
+       * @type gvr.lang.Block
+       */
+      this.block = new gvr.lang.Block(expressions);
+
+      /**
+       * The current step of the Do expression
+       * @type int
+       */
+      this.currentStep = 0;
+    },
+
+    /**
+     * Step into the do loop's block
+     * up to the number of times specified by {@link gvr.lang.Do#count}.
+     * Once the block has been entered this many times, the counter is
+     * reset so the process may occur again (for nested loops).
+     * @param globals global values passed throughout program execution.
+     */
+    step:function(globals){
+      gvr.lang.getRunner(globals).notify(this);
+      var stack = [];
+      if (this.currentStep < this.count){
+        stack.push(this);
+        this.currentStep++;
+        stack = stack.concat(this.block.step(globals));
+      } else {
+        this.currentStep = 0;
+      }
+      return stack;
+    }
+  });
+
+/**
+ * creates a new {@link gvr.lang.Do} object
+ * @returns {gvr.lang.Do}
+ */
+gvr.lang.newDo = function(line, count, expressions){
+  return new gvr.lang.Do(line, count, expressions);
+};
+
+
+
+gvr.lang.Define = gvr.lang.BaseExpression.extend(
+  /** @lends gvr.lang.Define# */
+  {
+    /**
+     * @class Represents a function definition in the gvr language.
+     * @extends gvr.lang.BaseExpression
+     * @constructs
+     * @param name The name of the function.  Should be unique for each definition.
+     *       Used by FunctionCall to look up the function.
+     * @param line See {@link gvr.lang.BaseExpression#line}
+     * @param expressions A list of expressions with which to
+     *          create a {@link gvr.lang.Block} object
+     */
+    init: function(line, name, expressions){
+      this._super(line, name);
+      /**
+       * The block to step into when this definition is called.
+       * @type gvr.lang.Block
+       */
+      this.block = new gvr.lang.Block(expressions);
+    },
+
+    /**
+     * Registers the function into the global namespace so it can be accessed
+     * by other language objects.
+     * @param globals global values passed throughout program execution.
+     */
+    step:function(globals){
+      gvr.lang.getRunner(globals).notify(this);
+      globals[this.name] = this;
+      return [];
+    }
+  }
+);
+
+/**
+ * creates a new {@link gvr.lang.Define} object
+ * @returns {gvr.lang.Define}
+ */
+gvr.lang.newDefine = function(line, name, expressions){
+  return new gvr.lang.Define(line, name, expressions);
+};
+
+gvr.lang.FunctionCall = gvr.lang.BaseExpression.extend(
+  /** @lends gvr.lang.FunctionCall# */
+  {
+    /**
+     * @class Represents a functionc all in the gvr language.
+     * @extends gvr.lang.BaseExpression
+     * @constructs
+     * @param line See {@link gvr.lang.BaseExpression#line}
+     * @param fname See {@link gvr.lang.FunctionCall#fname}
+     */
+    init: function(line, fname){
+      this._super(line, "call");
+      /**
+       * The name of the function to call
+       * @type String
+       */
+      this.fname = fname;
+    },
+    /**
+     * Steps into the block of the function stored in globals
+     * under the name specified by {@link gvr.lang.FunctionCall#fname}.
+     * @param globals global values passed throughout program execution.
+     */
+    step: function(globals){
+      gvr.lang.getRunner(globals).notify(this);
+      if (!globals[this.fname]){
+        throw new Error("The function "+this.fname+" is undefined.");
+      }
+      return globals[this.fname].block.step(globals);
+    }
+  });
+
+/**
+ * creates a new {@link gvr.lang.FunctionCall} object
+ * @returns {gvr.lang.FunctionCall}
+ */
+gvr.lang.newFunctionCall = function(line, fname){
+  return new gvr.lang.FunctionCall(line, fname);
+};
+
+module.exports = gvr.lang;
