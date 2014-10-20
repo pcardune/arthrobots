f57b4c88f3f2086bce7849b08f55837498de27cf
Paul Carduner <paul@carduner.net>
2014-10-20 14:32:34 -0700

    update devDependencies

--- src/main.js
+++ src/main.js
@@ -1,3 +1,42 @@
-var Parse = require('parse').Parse;
+/** @jsx React.DOM */
+var React = require('react');
+var Router = require('react-router');
+var Route = Router.Route;
+var Routes = Router.Routes;
+var NotFoundRoute = Router.NotFoundRoute;
+var DefaultRoute = Router.DefaultRoute;
+var Link = Router.Link;
 
-console.log("it works!")
+var LandingPage = React.createClass({
+  render: function() {
+    return (
+      <div>
+        hello there!
+      </div>
+    );
+  }
+});
+
+var AnthrobotApp = React.createClass({
+  render: function() {
+  	console.log("active route handler is", this.props.activeRouteHandler);
+    return (
+      <div>
+      	<h1 className="page-header">Arthrobots!</h1>
+
+        {/* this is the important part */}
+        {this.props.activeRouteHandler()}
+      </div>
+    );
+  }
+});
+
+var routes = (
+  <Routes location="history">
+    <Route name="app" path="/" handler={AnthrobotApp}>
+      <DefaultRoute handler={LandingPage}/>
+    </Route>
+  </Routes>
+);
+
+React.renderComponent(routes, document.body);
