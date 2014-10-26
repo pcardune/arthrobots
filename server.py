0de5ecd8f0b3b4ca9d805d335ecc7aa8fad42043
Paul Carduner <paul@carduner.net>
2014-10-25 18:05:42 -0700

    add a server that routes all non-static-resource urls to the index page to load the react app

---
+++ server.py
@@ -0,0 +1,16 @@
+import SimpleHTTPServer
+import SocketServer
+
+PORT = 8000
+
+class MyHTTPServer(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if "." not in self.path:
+            self.path = "/"
+        print "path is ", self.path
+        return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+httpd = SocketServer.TCPServer(("", PORT), MyHTTPServer)
+
+print "serving at port", PORT
+httpd.serve_forever()
