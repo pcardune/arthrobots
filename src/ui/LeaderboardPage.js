bd2ef1b606d1afd1cd420a88eb578a875d3609f2
Paul Carduner <paul@carduner.net>
2014-12-20 19:57:56 -0800

    better styling for leaderboard

--- src/ui/LeaderboardPage.js
+++ src/ui/LeaderboardPage.js
@@ -11,6 +11,7 @@ var NavItem = require('react-bootstrap').NavItem;
 
 var FBUtils = require('../FBUtils');
 
+require('./LeaderboardPage.css');
 var LeaderboardPage = React.createClass({
 
   getInitialState: function() {
@@ -52,11 +53,17 @@ var LeaderboardPage = React.createClass({
         return null;
       }
       return (
-        <div>
-          <Link to="profile" params={{userId:item.owner.id}}>
-            <span>{item.programCount}</span> - <span>{item.owner ? FBUtils.getUserName(item.owner) : "?"}</span>
-          </Link>
-        </div>
+        <tr>
+          <td>
+            <Link to="profile" params={{userId:item.owner.id}}>
+              <img className="gravatar" src={FBUtils.getProfilePic(item.owner)}/>
+              {item.owner ? FBUtils.getUserName(item.owner) : "?"}
+            </Link>
+          </td>
+          <td>
+            {item.programCount}
+          </td>
+        </tr>
       );
     });
     return (
@@ -65,8 +72,18 @@ var LeaderboardPage = React.createClass({
         </div>
         <div className="col-md-6">
           <h1>Leaderboard</h1>
-          <div>
-            {leaderboard}
+          <div className="panel panel-default">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Programmer</th>
+                <th># Programs Completed</th>
+              </tr>
+            </thead>
+            <tbody>
+              {leaderboard}
+            </tbody>
+          </table>
           </div>
         </div>
       </div>
