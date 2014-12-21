3400492a82fe2858b42bd8756758cc34907b829e
Paul Carduner <paul@carduner.net>
2014-12-21 10:04:20 -0800

    use fbProfile data instead of authData for profile pic since authData is only available for logged in user

--- src/FBUtils.js
+++ src/FBUtils.js
@@ -2,9 +2,9 @@ var gravatar = require('gravatar');
 
 var FBUtils = {
   getProfilePic: function(user) {
-    var authData = user.get('authData');
-    if (authData && authData.facebook) {
-      return 'http://graph.facebook.com/'+authData.facebook.id+'/picture?type=square&width=170&height=170'
+    var fbProfile = user.get('fbProfile');
+    if (fbProfile && fbProfile.id) {
+      return 'http://graph.facebook.com/'+fbProfile.id+'/picture?type=square&width=170&height=170'
     } else {
       return gravatar.url(user.get('email'), {s:170});
     }
